daad1f07e3c812079140083ebbe494ace4b79613
emrek <emrek@DESKTOP-JN2GG86>
2022-10-27 22:51:02 +0300

    Add project files.

---
+++ DataAccess/EntityFramework/RecyleContex.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.EntityFramework
+{
+    public class RecyleContex:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = (localdb)\mssqllocaldb; Database = Northwind; Trusted_Connection = true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
